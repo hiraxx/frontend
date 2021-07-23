fdbd80a42497a8028c1432e764ca4c1e43f28784
hiraxx <colendrespatricia@gmail.com>
2021-07-23 15:03:16 +0800

    first commit

---
+++ components/BooksView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button class="btn btn-success float-right">
+      New Book
+    </button>
+    <h5>Asset</h5>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container mt-5">
+      <h1 class="text-center">
+        <span>Welcome</span>
+        <span v-if="$auth.loggedIn">
+          {{$auth.user.name}}
+        </span>
+      </h1>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
+
+<style>
+</style>
